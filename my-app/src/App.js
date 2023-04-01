cd31c19157b90a80189821794438f640ee3e21cf
KCSN_Aun <s6204062620101@email.kmutnb.ac.th>
2023-04-01 09:18:26 +0700

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,75 +1,79 @@
-import { useState, useEffect } from 'react';
-import './Sidebardata.css';
-import { BrowserRouter, NavLink, Routes, Route } from 'react-router-dom'
-import Reportrepair from './reportrepair';
-import Home from './Home';
-import Repair from './repair';
-import Room from './room';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+  useLocation
+} from "react-router-dom";
 
-function Sidebardata() {
-  const displayname = sessionStorage.getItem('displayname');
-  const [repairType, setRepairType] = useState('');
+// You can use the last <Route> in a <Switch> as a kind of
+// "fallback" route, to catch 404 errors.
+//
+// There are a few useful things to note about this example:
+//
+// - A <Switch> renders the first child <Route> that matches
+// - A <Redirect> may be used to redirect old URLs to new ones
+// - A <Route path="*> always matches
 
+export default function NoMatchExample() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/old-match">Old Match, to be redirected</Link>
+          </li>
+          <li>
+            <Link to="/will-match">Will Match</Link>
+          </li>
+          <li>
+            <Link to="/will-not-match">Will Not Match</Link>
+          </li>
+          <li>
+            <Link to="/also/will/not/match">Also Will Not Match</Link>
+          </li>
+        </ul>
 
-  useEffect(() => {
-    const storageRepairType = sessionStorage.getItem('repairType');
-    if (storageRepairType) {
-      setRepairType(storageRepairType);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (window.location.pathname.startsWith('/repair')) {
-      const type = window.location.pathname.split('/')[2];
-      setRepairType(type);
-      sessionStorage.setItem('repairType', type);
-    }
-  }, [window.location]);
-  const handleLogout = () => {
-    sessionStorage.clear();
-    window.location.reload();
-  }
-  
-  if(!displayname){
-    return (
-      <BrowserRouter>
-        <div class="sidenav">
-        <a class="navbar-brand" ><NavLink to="/"></NavLink></a>
-          <a class="navbar-brand" ><NavLink to={`/repair/${repairType}`}>แจ้งซ่อม</NavLink></a>
-          <a class="navbar-brand" ><NavLink to="/room">แจ้งย้าย</NavLink></a>
-          <a class="navbar-brand" ><NavLink to="/reportrepair">แจ้ง</NavLink></a>
-        </div>
-        <div class="content">
-          <Routes>
-            <Route path='/' element={<Room />}></Route>
-            <Route path='/repair/:type' element={<Repair />}></Route>
-            <Route path='/room' element={<Room />}></Route>
-            <Route path='/reportrepair' element={<Reportrepair />}></Route>
-          </Routes>
-        </div>
-      </BrowserRouter>
-    )
-  }
-  else{
-    return (
-      <BrowserRouter>
-        <div class="sidenav">
-          <a class="navbar-brand" ><NavLink to={`/repair/${repairType}`}>แจ้งซ่อม</NavLink></a>
-          <a class="navbar-brand" ><NavLink to="/room">แจ้งย้าย</NavLink></a>
-          <a class="navbar-brand" ><NavLink to="/reportrepair">แจ้ง</NavLink></a>
-          <a class="navbar-brand" onClick={handleLogout}><NavLink to="/signin">LogOut</NavLink></a>
-        </div>
-        <div class="content">
-          <Routes>
-            <Route path='/repair/:type' element={<Repair />}></Route>
-            <Route path='/room' element={<Room />}></Route>
-            <Route path='/reportrepair' element={<Reportrepair />}></Route>
-          </Routes>
-        </div>
-      </BrowserRouter>
-    )
-  }
-  
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/old-match">
+            <Redirect to="/will-match" />
+          </Route>
+          <Route path="/will-match">
+            <WillMatch />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default Sidebardata;
+function Home() {
+  return <h3>Home</h3>;
+}
+
+function WillMatch() {
+  return <h3>Matched!</h3>;
+}
+
+function NoMatch() {
+  let location = useLocation();
+
+  return (
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
+    </div>
+  );
+}
