c001994143d9a42bd9d9c69697d8f9957bbfb24a
KCSN_Aun <s6204062620101@email.kmutnb.ac.th>
2023-04-07 04:16:50 +0700

    test

--- src/component/repair.js
+++ src/component/repair.js
@@ -1,129 +1,34 @@
-import React,{ useState, useEffect, useRef } from "react";
-import axios from 'axios';
+import React, { useState, useRef } from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
-export default function Repair(){
-  const [material, setMaterial] = useState([]);
-  const storageRepairType = sessionStorage.getItem('repairType');
-  const durablearticles_Id = storageRepairType;
-  const [room, setRoom] = useState('');
-  const [description, setDescription] = useState('');
-  const [Informer, setInformer] = useState('');
-  const [du_name, setDurablearticlesName] = useState('');
-  const [typeId, setTypeDurablearticlesId] = useState('');
-  const repair_durablearticles_Id=" ";
-  useEffect(() => {
-    const getMaterial = async () => {
-      const response = await axios.get('http://localhost:3001/durablearticles');
-      const jsonData = response.data;
-      setMaterial(jsonData);
-      const foundData = jsonData.find(data => data.durablearticles_Id === durablearticles_Id);
-      if (foundData) {
-        setDurablearticlesName(foundData.durablearticles_name);
-        setTypeDurablearticlesId(foundData.type_durablearticles_Id);
-      }
-    };
-    getMaterial();
-  }, [durablearticles_Id]);
-  
-  const submitRepair = async () => {
-    if (!Informer) {
-      alert('Please fill in the informer field.');
-      return;
-    }
+const CameraScreen = () => {
+  const cameraRef = useRef(null);
+  const [cameraType, setCameraType] = useState(RNCamera.Constants.Type.back);
 
-    const data = {
-      repair_durablearticles_Id,
-      du_name,
-      durablearticles_Id,
-      room,
-      description,
-      Informer,
-      repair_detail: description, 
-      Informer: Informer, 
+  const switchCamera = () => {
+    if (cameraType === RNCamera.Constants.Type.back) {
+      setCameraType(RNCamera.Constants.Type.front);
+    } else {
+      setCameraType(RNCamera.Constants.Type.back);
     }
-    console.log(description)
-    try {
-      const response = await axios.post('http://localhost:3001/repairs', data);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    submitRepair();
-  }
-  
-  const handleInputChange =(event) => {
-    event.preventDefault();
-    const { name, value } = event.target;
-    if (name === 'room') {
-      setRoom(value);
-    } else if (name === 'description') {
-      setDescription(value);
-    } else if (name === 'Informer') {
-      setInformer(value);
-    }
-  }
-  const videoRef = useRef(null);
-  const [stream, setStream] = useState(null);
-  
-  const handleStream = (stream) => {
-    setStream(stream);
-    if (videoRef.current) {
-      videoRef.current.srcObject = stream;
-    }
-  }
-  
-  const handleCameraError = (error) => {
-    console.error('Unable to access camera: ', error);
-  }
-  
-  const startCamera = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
-      handleStream(stream);
-    } catch (error) {
-      handleCameraError(error);
-    }
-  }
-  
-  useEffect(() => {
-    startCamera();
-    return () => {
-      if (stream) {
-        stream.getTracks().forEach((track) => {
-          track.stop();
-        });
-      }
-    }
-  }, []);
-  return(
-    <div>
-      <h2>แจ้งซ่อมครุภัณฑ์</h2>
-      <form onSubmit={handleSubmit}>
-          <label>ID:{durablearticles_Id}  </label><br /><br />
-          <label>ชื่อ:{du_name} </label><br /><br />
-          <label>ประเภท:{typeId} </label><br /><br />
-          <label>ห้อง:</label>
-          <select name="room" value={room} onChange={handleInputChange}>
-            <option value="select">select</option>
-            <option value="78-601">78-601</option>
-          </select><br /><br />
-          {stream && (
-        <div>
-          <video ref={videoRef} autoPlay />
-        </div>
-      )}
-          <label>รายละเอียดเพิ่มเติม:</label>
-          <input type="text" onChange={handleInputChange} value={description} name="description" />
-          <br /><br />
-          <label>ผู้แจ้ง:</label>
-          <input type="text" onChange={handleInputChange} value={Informer} name="Informer" /><br />
-          <br /><br />
-          <button type="submit" className="submit" onClick={handleSubmit}>Submit</button>
-      </form>
-    </div>
-  )
-}
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <RNCamera
+        ref={cameraRef}
+        style={{ flex: 1 }}
+        type={cameraType}
+        autoFocus={RNCamera.Constants.AutoFocus.on}
+        flashMode={RNCamera.Constants.FlashMode.off}
+      />
+
+      <TouchableOpacity onPress={switchCamera}>
+        <Text>Switch Camera</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default CameraScreen;
